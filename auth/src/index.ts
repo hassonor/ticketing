2d4fd54a2da9947ed2aa1af06eabf139b97927c8
Or Hasson <hassonor@gmail.com>
2024-08-07 14:09:48 +0300

    add init auth routes + express validator package

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,18 @@
 import express from 'express';
 import {json} from 'body-parser';
+import {currentUserRouter} from "./routes/current-user";
+import {signInRouter} from "./routes/signin";
+import {signOutRouter} from "./routes/signout";
+import {signUpRouter} from "./routes/signup";
 
 
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser',(req,res)=>{
-    res.send('Hi there');
-})
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
 
 app.listen(4000, ()=>{
     console.log('Listening on port 4000');
---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,23 @@
+import express,{Response,Request} from "express";
+import {body, validationResult} from 'express-validator';
+
+const router = express.Router();
+
+router.post('/api/users/signup',[
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().isLength({min: 4, max: 20})
+        .withMessage('Password must be at least 4 and 20 characters'),
+],(req: Request,res:Response)=>{
+    const  errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).send({errors: errors.array()});
+    }
+
+    const {email: password} = req.body;
+    console.log('Creating a user...');
+
+    res.send({})
+
+});
+
+export {router as signUpRouter};
