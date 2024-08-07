7ae08e5c576290a164f006cc3624bb5c9f260eed
Or Hasson <hassonor@gmail.com>
2024-08-07 13:35:59 +0300

    setup ingress

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ import {json} from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser',(req,res)=>{
+    res.send('Hi there');
+})
+
 app.listen(4000, ()=>{
     console.log('Listening on port 4000');
 });
