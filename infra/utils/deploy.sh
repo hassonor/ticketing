1fdce7aac7e4add0f01d4efc82e76e58541b8a1c
Or Hasson <hassonor@gmail.com>
2024-08-08 14:42:25 +0300

    add license

---
+++ infra/utils/deploy.sh
@@ -0,0 +1,6 @@
+gnome-terminal --tab -- sh -c "cd client && npm start; exec bash"
+gnome-terminal --tab -- sh -c "cd comments && npm start; exec bash"
+gnome-terminal --tab -- sh -c "cd event-bus && npm start; exec bash"
+gnome-terminal --tab -- sh -c "cd moderation && npm start; exec bash"
+gnome-terminal --tab -- sh -c "cd posts && npm start; exec bash"
+gnome-terminal --tab -- sh -c "cd query && npm start; exec bash"
