bc5175cbed837b4c837f4f8c6c02a7a19eac647f
Or Hasson <hassonor@gmail.com>
2024-08-08 15:25:00 +0300

    add more tests

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import {app} from '../../app';
 import {User} from '../../models/users';
+import {PasswordManager} from '../../services/password';
 
 it('fails when a non-existent email is supplied', async () => {
     await request(app)
@@ -15,7 +16,8 @@ it('fails when a non-existent email is supplied', async () => {
 it('fails when an incorrect password is supplied', async () => {
     const email = 'test@test.com';
     const password = 'password';
-    const user = User.build({email, password});
+    const hashedPassword = await PasswordManager.toHash(password);
+    const user = User.build({email, password: hashedPassword});
     await user.save();
 
     await request(app)
@@ -26,7 +28,6 @@ it('fails when an incorrect password is supplied', async () => {
         })
         .expect(400);
 });
-
 
 it('returns a 400 with an invalid email', async () => {
     await request(app)
@@ -58,4 +59,21 @@ it('returns a 400 with missing email and password', async () => {
         .post('/api/users/signin')
         .send({password: 'password'})
         .expect(400);
+});
+
+it('sets a cookie after successful signin', async () => {
+    const email = 'test@test.com';
+    const password = 'password';
+    const user = User.build({email, password});
+    await user.save();
+
+    const response = await request(app)
+        .post('/api/users/signin')
+        .send({
+            email: email,
+            password: password
+        })
+        .expect(200);
+
+    expect(response.get('Set-Cookie')).toBeDefined();
 });
--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,5 +1,7 @@
 import request from 'supertest';
 import {app} from '../../app';
+import cookieSession from "cookie-session";
+
 
 it('returns a 201 on successful signup', async () => {
     await request(app)
@@ -53,4 +55,18 @@ it('disallows duplicate emails', async () => {
         })
         .expect(400);
 });
+
+
+it('sets a cookie after a successful signup', async () => {
+
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'hassonor@gmail.com',
+            password: 'password'
+        })
+        .expect(201);
+
+    expect(response.get('Set-Cookie')).toBeDefined();
+})
 
