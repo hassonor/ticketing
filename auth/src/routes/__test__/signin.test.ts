202c2a819464ce4a7997926744394a3d40e8902b
Or Hasson <hassonor@gmail.com>
2024-08-08 15:28:34 +0300

    improve signin tests

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -14,16 +14,18 @@ it('fails when a non-existent email is supplied', async () => {
 });
 
 it('fails when an incorrect password is supplied', async () => {
-    const email = 'test@test.com';
-    const password = 'password';
-    const hashedPassword = await PasswordManager.toHash(password);
-    const user = User.build({email, password: hashedPassword});
-    await user.save();
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'hassonor@gmail.com',
+            password: 'password'
+        })
+        .expect(201);
 
     await request(app)
         .post('/api/users/signin')
         .send({
-            email: email,
+            email: 'hassonor@gmail.com',
             password: 'incorrectpassword'
         })
         .expect(400);
@@ -61,17 +63,32 @@ it('returns a 400 with missing email and password', async () => {
         .expect(400);
 });
 
+it('returns a 400 with invalid email and password', async () => {
+    await request(app)
+        .post('/api/users/signin')
+        .send({email: 'testtest.com', password: 'password'})
+        .expect(400);
+
+    await request(app)
+        .post('/api/users/signin')
+        .send({email: 'test@test.com', password: '1'})
+        .expect(400);
+});
+
 it('sets a cookie after successful signin', async () => {
-    const email = 'test@test.com';
-    const password = 'password';
-    const user = User.build({email, password});
-    await user.save();
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'hassonor@gmail.com',
+            password: 'password'
+        })
+        .expect(201);
 
     const response = await request(app)
         .post('/api/users/signin')
         .send({
-            email: email,
-            password: password
+            email: 'hassonor@gmail.com',
+            password: 'password'
         })
         .expect(200);
 
