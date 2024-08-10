64a1ca11add1d30f4ffbe34d66a334c98d94dffa
Or Hasson <hassonor@gmail.com>
2024-08-10 21:49:20 +0300

    add create ticket saving + test

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import {app} from '../../app';
+import {Ticket} from "../../models/tickets";
 
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
@@ -57,12 +58,23 @@ it('returns an error if an invalid price is provided', async () => {
 })
 
 it('create a ticket with valid inputs', async () => {
-    // Add in a check to make sure a ticket was saved
+    let tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(0);
+
+    const title = "Some Title For The New Ticket";
+    const price = 25;
+
     await request(app)
         .post('/api/tickets')
+        .set('Cookie', global.signin())
         .send({
-            title: 'Some Title',
-            price: 25
+            title,
+            price,
         })
         .expect(201);
+
+    tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(1);
+    expect(tickets[0].title).toEqual(title);
+    expect(tickets[0].price).toEqual(price);
 })
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from "express";
 import {requireAuth, validateRequest} from "@ohticketing/common";
 import {body} from "express-validator";
+import {Ticket} from "../models/tickets";
 
 
 const router = express.Router();
@@ -9,8 +10,16 @@ const router = express.Router();
 router.post('/api/tickets', requireAuth, [
     body('title').not().isEmpty().withMessage('Title is required'),
     body('price').isFloat({gt: 0}).withMessage('Price must be greater than zero')
-], validateRequest, (req: Request, res: Response) => {
-    res.sendStatus(200);
+], validateRequest, async (req: Request, res: Response) => {
+    const {title, price} = req.body;
+    const ticket = Ticket.build({
+        title,
+        price,
+        userId: req.currentUser!.id
+    })
+
+    await ticket.save();
+    res.status(201).send(ticket);
 })
 
 export {router as createTicketRouter};
