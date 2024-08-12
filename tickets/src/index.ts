e9e0041d9639d30c351abe4a300299f4cb2b2a00
Or Hasson <hassonor@gmail.com>
2024-08-13 00:47:48 +0300

    revert kafka will need to find smoother solution to setup

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import {app} from "./app";
-import {kafkaWrapper} from "@ohticketing/common";
+// import {kafkaWrapper} from "@ohticketing/common";
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
@@ -9,13 +9,13 @@ const start = async () => {
     if (!process.env.MONGO_URI) {
         throw new Error('MONGO_URI must be defined');
     }
-    try {
-        await kafkaWrapper.connect(['my-cluster-kafka-bootstrap:9092'], 'tickets-service');
-        await kafkaWrapper.producer.connect();
-        console.log('Connected to Kafka');
-    } catch (err) {
-        console.error(err);
-    }
+    // try {
+    //     await kafkaWrapper.connect(['my-cluster-kafka-bootstrap:9092'], 'tickets-srv');
+    //     await kafkaWrapper.producer.connect();
+    //     console.log('Connected to Kafka');
+    // } catch (err) {
+    //     console.error(err);
+    // }
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to MongoDB');
